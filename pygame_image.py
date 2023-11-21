de95b6b320c4fda6a53d4370c47a5620bc2e38e1
c0a22049 <c0a220493f@edu.teu.ac.jp>
2023-11-21 15:40:03 +0900

    練習3

--- pygame_image.py
+++ pygame_image.py
@@ -9,13 +9,14 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex01/fig/3.png") #練習2：こうかとん画像のSurface
     kk_img = pg.transform.flip(kk_img, True, False) #練習2：こうかとん左右反転
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 90, 1.0)]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0]) #練習4：背景画像表示
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_imgs[1], [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
